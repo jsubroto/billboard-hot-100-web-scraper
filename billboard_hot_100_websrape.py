9c41b0c9d5829ca5298ae4edac25c9691676c841
Jaimes Subroto <jaimes_subroto@yahoo.com>
2018-04-15 21:29:26 -0700

    Prints each song's position, name, and artist

--- billboard_hot_100_websrape.py
+++ billboard_hot_100_websrape.py
@@ -11,11 +11,26 @@ uClient.close() # Close the client
 # HTML parsing
 page_soup = soup(page_html, "html.parser")
 
-# Prints the HTML contents
-print(page_soup)
-
 # Grabs divs, each containing the song and artist name
 containers = page_soup.findAll('div', {'class': 'chart-row__title'})
 
-# Prints the length of containers
-print(len(containers)) # 100
+chart_position = 1
+
+# Loops through each container
+for container in containers:
+
+    # Grabs the song name
+    song = container.h2.text
+    
+    # Grabs the artist name
+    try:
+        artist = container.a.text.strip()
+    except AttributeError:
+        artist = container.span.text.strip()
+
+    # Prints the chart position, song name, and artist name
+    print('\nPosition: #{}'.format(chart_position))
+    print('Song: {}'.format(song))
+    print('Artist: {}'.format(artist))
+
+    chart_position += 1
