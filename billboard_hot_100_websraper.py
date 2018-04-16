c83e79f2b2cb2b09cee9d47b85f88839e0bac05e
Jaimes Subroto <jaimes_subroto@yahoo.com>
2018-04-15 22:47:17 -0700

    Added the option not to print each song to the console

--- billboard_hot_100_websraper.py
+++ billboard_hot_100_websraper.py
@@ -22,7 +22,20 @@ headers = 'Song, Artist, Last Week, Peak Position, Weeks on Chart\n'
 f.write(headers)
 
 print('Welcome to Jaimes Subroto\'s Billboard Hot 100 Python Web Scraper!')
-print('Printing this week\'s HOTTEST 100 songs...')
+
+# Asks the user if he/she wants the data to be printed to the console
+while True:
+    print_data = input('Would you like the data to be printed to the console? ')
+    if print_data.lower() == 'yes' or print_data.lower() == 'y':
+        print_data = True
+        print('\nPrinting this week\'s HOTTEST 100 songs...')
+        break
+    elif print_data.lower() == 'no' or print_data.lower() == 'n':
+        print_data = False
+        print('\nScraping this week\'s HOTTEST 100 songs...')
+        break
+    else:
+        print('Sorry, I didn\'t get that.')
 
 chart_position = 1
 
@@ -54,12 +67,13 @@ for container in containers:
     weeks_on_chart = weeks_on_chart_container.find('span', {'class': 'chart-row__value'}).text
 
     # Prints the chart position, song name, artist name, and related stats
-    print('\nPosition: #{}'.format(chart_position))
-    print('Song: {}'.format(song))
-    print('Artist: {}'.format(artist))
-    print('Last Week: {}'.format(last_week))
-    print('Peak Position: {}'.format(peak_position))
-    print('Weeks on Chart: {}'.format(weeks_on_chart))
+    if print_data:
+        print('\nPosition: #{}'.format(chart_position))
+        print('Song: {}'.format(song))
+        print('Artist: {}'.format(artist))
+        print('Last Week: {}'.format(last_week))
+        print('Peak Position: {}'.format(peak_position))
+        print('Weeks on Chart: {}'.format(weeks_on_chart))
 
     chart_position += 1
 
