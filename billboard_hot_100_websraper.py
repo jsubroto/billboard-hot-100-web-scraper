6caf84c6dec03526c7c1ce1c211ae7818b5e4611
Jaimes Subroto <jaimessubroto@gmail.com>
2024-09-21 23:40:53 -0700

    Refactored the code and switched to requests

--- billboard_hot_100_websraper.py
+++ billboard_hot_100_websraper.py
@@ -1,18 +1,15 @@
-from urllib.request import urlopen as uRequest
-from bs4 import BeautifulSoup as soup
+import requests
+from bs4 import BeautifulSoup
 
-url = "https://www.billboard.com/charts/hot-100"
-
-# Opening up connection, grabbing the page
-uClient = uRequest(url)
-page_html = uClient.read()  # Offloads content into a variable
-uClient.close()  # Close the client
+url = "https://www.billboard.com/charts/hot-100/"
+response = requests.get(url)
 
 # HTML parsing
-page_soup = soup(page_html, "html.parser")
+soup = BeautifulSoup(response.text, "html.parser")
 
 # Grabs all information related to the top 100 songs
-containers = page_soup.select('article[class*=chart]')  # *= means contains
+list_class = "ul.o-chart-results-list-row"
+containers = soup.select(list_class)
 
 filename = "billboard_hot_100.csv"
 f = open(filename, 'w')  # w = write
@@ -42,36 +39,11 @@ chart_position = 1
 
 # Loops through each container
 for container in containers:
+    song = container.find("h3", {"class": "c-title"}).text.strip()
+    artist = container.find("span", {"class": "a-no-trucate"}).text.strip()
 
-    # Container storing the song name and artist name
-    song_container = container.find("div", {"class": "chart-row__title"})
-
-    # Grabs the song name
-    song = song_container.h2.text
-
-    # Grabs the artist name
-    try:
-        artist = song_container.a.text.strip()
-    except AttributeError:
-        artist = song_container.span.text.strip()
-
-    # Grabs the song's position last week
-    last_week_container = container.find(
-        "div", {"class": "chart-row__last-week"})
-    last_week = last_week_container.find(
-        "span", {"class": "chart-row__value"}).text
-
-    # Grabs the song's peak position
-    peak_position_container = container.find(
-        "div", {"class": "chart-row__top-spot"})
-    peak_position = peak_position_container.find(
-        "span", {"class": "chart-row__value"}).text
-
-    # Grabs the song's duration in the hot 100 (in weeks)
-    weeks_on_chart_container = container.find(
-        "div", {"class": "chart-row__weeks-on-chart"})
-    weeks_on_chart = weeks_on_chart_container.find(
-        "span", {"class": "chart-row__value"}).text
+    last_week, peak_position, weeks_on_chart = container.find_all(
+        "ul", {"class": "lrv-a-unstyle-list"})[-1].text.strip().split()
 
     # Prints the chart position, song name, artist name, and related stats
     if print_data:
