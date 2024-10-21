6a3abbb14fce8735939071c4fe1a013d9c1fdfed
BookCatKid <99609593+BookCatKid@users.noreply.github.com>
2024-10-21 10:23:09 -0700

    Adding images and status (#3)
    * Adding images and status
    
    Added some functionality for getting images, and the status svg. (I didn't do rewards as I don't really understand them).
    I also added an html file used for viewing the downloaded csv.
    
    * Rename the copy
    
    * Improve README.md
    
    Added instructions from start.md
    
    * Reset original scraper and remove display_csv.py
    
    There are two version of the scraper now if somebody doesn't want images/svgs. I also removed display_csv.py as it does not really work and would be a pain to implement (html is better)
    
    * Rename to status (oops)
    
    * Fix new and re-entry tags in status
    
    This bug has been fixed!
    
    * Added a custom server to prevent caching
    
    While testing I was having some caching problems, so I added a custom http server that removes that, and added a section to the README.
    
    * Remove Comments
    
    * Rename display_csv to index.html
    
    Now you can access it from root (http://localhost:8000/)

--- billboard_hot_100_websraper_images_status.py
+++ billboard_hot_100_websraper_images_status.py
@@ -9,15 +9,13 @@ soup = BeautifulSoup(response.text, "html.parser")
 filename = "billboard_hot_100.csv"
 f = open(filename, 'w')  # w = write
 
-headers = "Song, Artist, Last Week, Peak Position, Weeks on Chart\n"
-
+headers = "Image, Status, Song, Artist, Last Week, Peak Position, Weeks on Chart\n"
 f.write(headers)
 
 print("Welcome to Jaimes Subroto's Billboard Hot 100 Python Web Scraper!")
 
 while True:
-    print_data = input(
-        "Would you like the data to be printed to the console? ")
+    print_data = input("Would you like the data to be printed to the console? ")
     if print_data.lower() in {"yes", 'y'}:
         print_data = True
         print("\nPrinting this week's HOTTEST 100 songs...")
@@ -36,6 +34,22 @@ for i, container in enumerate(soup.select("ul.o-chart-results-list-row")):
     last_week, peak_position, weeks_on_chart = container.find_all(
         "ul", {"class": "lrv-a-unstyle-list"})[-1].text.strip().split()
 
+    # Get the image URL from the img element
+    image_url = container.find("img")["src"] if container.find("img") else "N/A"
+
+    new_span = container.find_all("span", {"class": "u-background-color-yellow"})
+    if len(new_span) > 0:
+        new_text = new_span[0].text.strip()
+        if new_text.endswith("ENTRY"):
+            new_text = "RE-ENTRY"
+            status_svg_url = f'<span style="font-family: \'Arial\', sans-serif; font-weight: bold; font-size: 0.8rem; color: #4a4a4a; background-color: #ffeb3b; text-align: center; padding: 10px">{new_text}</span>'
+        else:
+            status_svg_url = f'<span style="font-family: \'Arial\', sans-serif; font-weight: bold; font-size: 1.5rem; color: #4a4a4a; background-color: #ffeb3b; text-align: center; padding: 10px">{new_text}</span>'
+
+    else:
+        status_svgs = container.find_all("svg") 
+        status_svg_url = status_svgs[1]
+
     if print_data:
         print(f"\nPosition: #{i + 1}")
         print(f"Song: {song}")
@@ -43,9 +57,10 @@ for i, container in enumerate(soup.select("ul.o-chart-results-list-row")):
         print(f"Last Week: {last_week}")
         print(f"Peak Position: {peak_position}")
         print(f"Weeks on Chart: {weeks_on_chart}")
+        print(f"Image URL: {image_url}")
+        print(f"Status SVG: {status_svg_url}")
 
-    f.write('\"' + song + '\",\"' + artist.replace("Featuring", "Feat.") +
-            '\",' + last_week + ',' + peak_position + ',' + weeks_on_chart + '\n')
+    f.write(f'\"{image_url}\",\"{status_svg_url}\",\"{song}\",\"{artist.replace("Featuring", "Feat.")}\",{last_week},{peak_position},\"{weeks_on_chart}\"\n')
 
 f.close()
 
